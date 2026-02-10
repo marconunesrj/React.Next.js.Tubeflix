5ce6363285542df450edd7d126ab09c1630fe930
MARCONUNES\marco <marconunes.rj@hotmail.com>
2026-02-10 16:00:56 -0300

    Layout Base do App (layout.module.css)

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,9 @@
-import MusicsPage from "./musics/page";
-import VideosPage from "./videos/page";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
       <h2 className={styles.title}>Início</h2>
-      <VideosPage />
-      <MusicsPage />
     </>
   );
 }
